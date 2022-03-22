f3295053e7ef5aaef1a517e363d5dc46ef9de24e
Osmar Junior <86473856+Firulinha@users.noreply.github.com>
2022-03-22 20:32:33 -0300

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#String
+
+print("123" + "254")
+print("Hello"[0]) #Utilizar [0] mostrará a letra H.and
+print("Hello"[3])
+
+#Interger
+
+print(123+254)
+
+#Float
+
+print(7/3)
+
+#Boolean -> True or False
+
